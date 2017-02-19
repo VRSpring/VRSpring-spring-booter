6ee6f7fea737973d86a44b962fa2a4d403654d09
xiejiangchu <1235116@tongji.edu.cn>
2017-02-19 13:20:05 +0800

    pc站点

---
+++ src/main/java/com/vrspring/web/controller/HomeController.java
@@ -0,0 +1,27 @@
+package com.vrspring.web.controller;
+
+import com.vrspring.web.bean.HomeBean;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+/**
+ * @Author xie
+ * @Date 17/2/19 上午10:15.
+ */
+@Controller
+public class HomeController {
+
+    @RequestMapping("/index")
+    public String index(HttpServletRequest request){
+        return "index";
+    }
+
+    @RequestMapping("/about")
+    public String about(HttpServletRequest request){
+        return "about";
+    }
+}
